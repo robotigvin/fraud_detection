c984a323fd2b5d075666a9311a447c2fe375dac0
robotigvin <vinaypai1977@gmail.com>
2022-08-01 01:49:00 +0530

    adding m1 m2 m3

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 #model.py
+print("adding all ML related code here")
+
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+    print("adding code for model 2")
+
+def model3():
+    print("adding code for model 3")
