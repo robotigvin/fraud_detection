cce5ad90cf2cc0ff397183efc7346dc0f2fa2d70
robotigvin <vinaypai1977@gmail.com>
2022-08-01 00:00:30 +0530

    added a code that gives wrong result

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("adding all the data processing code related to fraud detection")
+a=5
+b=10
+
+def summer(a,b):
+    c=a+b
+    return a
+
+print(summer(a,b))
+print("this code is incorrect. we need to return the sum but it's showing us the 1st variable")
